ae393aa676d8d80cb5016f21ac317fbccb55787c
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2024-02-10 03:04:54 +0530

    material react table added

--- src/components/SearchEntity.js
+++ src/components/SearchEntity.js
@@ -1,257 +1,169 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Modal from "@mui/material/Modal";
-import Paper from "@mui/material/Paper";
-import { Field, Form, Formik } from "formik";
-import { ThemeProvider, createTheme, useTheme } from "@mui/material/styles";
-import moment from "moment";
-import TextField from "@mui/material/TextField";
-import TableContainer from "@mui/material/TableContainer";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import Fade from "@mui/material/Fade";
+import { useEffect, useMemo, useState } from "react";
 import {
-  Box,
-  Button,
-  Divider,
-  Grid,
-  InputLabel,
-  Typography,
-} from "@mui/material";
-// import { commonStyle } from "../../assets/style/js/commonStyle";
-import CustomTable from "../components/customizeTable/CustomTable";
-// import CoustomButton from "../../../../components/CustomButtons/Button";
-import tableStyle from "../components/customizeTable/style";
-import get from "lodash/get";
-import isEmpty from "lodash/isEmpty";
-import { makeStyles } from "@mui/material/styles";
-import { connect } from "react-redux";
-import { useSnackbar } from "notistack";
-import CloseIcon from "@mui/icons-material/Close";
-// import { TablePageData } from "../../utils/constants";
-const TablePageData = {
-  lastPage: 1,
-  from: 0,
-  to: 5,
-  total: 0,
-  perPageOptions: [5, 10, 25],
-};
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+const SearchEntity = () => {
+  //data and fetching state
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefetching, setIsRefetching] = useState(false);
+  const [rowCount, setRowCount] = useState(0);
 
-const useStyles = makeStyles(tableStyle);
+  //table state
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [sorting, setSorting] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
 
-const headCellsItems = [
-  {
-    id: "ProcessorName",
-    label: "Processor",
-    isSort: true,
-    sortProperty: "ProcessorName",
-    actionsCellStyle: "left",
-  },
-  {
-    id: "LastCalcMonthYear",
-    label: "Last Calculation Date",
-    isSort: true,
-    sortProperty: "LastCalcMonthYear",
-    actionsCellStyle: "center",
-  },
-  {
-    id: "Status",
-    label: "Status",
-    isSort: true,
-    sortProperty: "Status",
-    actionsCellStyle: "center",
-  },
-];
+  //if you want to avoid useEffect, look at the React Query example instead
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!data.length) {
+        setIsLoading(true);
+      } else {
+        setIsRefetching(true);
+      }
 
-const SearchEntity = ({ payout }) => {
-  const classes = useStyles();
-  const oldTheme = useTheme();
-  const { enqueueSnackbar } = useSnackbar();
-  const [open, setOpen] = React.useState(false);
-  const [lastCalcData, setLastCalculationData] = React.useState([]);
-  const [page, setPage] = React.useState(1);
-  const [initialCall, setInitialCall] = React.useState(true);
-  const [pageDetails, setPageDetails] = React.useState({ ...TablePageData });
-  const [order, setOrder] = React.useState("desc");
-  const [orderBy, setOrderBy] = React.useState("ProcessorName");
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+      // const url = new URL(
+      //   '/api/data',
+      //   process.env.NODE_ENV === 'production'
+      //     ? 'https://www.material-react-table.com'
+      //     : 'http://localhost:3000',
+      // );
+      // url.searchParams.set(
+      //   'start',
+      //   `${pagination.pageIndex * pagination.pageSize}`,
+      // );
+      // url.searchParams.set('size', `${pagination.pageSize}`);
+      // url.searchParams.set('filters', JSON.stringify(columnFilters ?? []));
+      // url.searchParams.set('globalFilter', globalFilter ?? '');
+      // url.searchParams.set('sorting', JSON.stringify(sorting ?? []));
 
-  const handleChangeRowsPerPage = (event) => {
-    setPage(1);
-    setRowsPerPage(event.target.value);
-  };
+      try {
+        var myHeaders = new Headers();
+        myHeaders.append(
+          "Authorization",
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzUyOWI4MGM2NWE3MjgzNzQ1ZjE5ZCIsImlhdCI6MTcwNzQyMjk5OH0.iLWqOHbJ53LAJdgsNniw6_KBpC3iJidN0F7AufOQ0JI"
+        );
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+        var requestOptions = {
+          method: "GET",
+          headers: myHeaders,
+          redirect: "follow",
+        };
 
-  const theme = createTheme({
-    ...oldTheme,
-    overrides: {
-      MuiAutocomplete: {
-        inputRoot: {
-          padding: "0px",
-        },
-        input: {
-          padding: "0px",
-        },
+        const response = await fetch(
+          "http://localhost:3000/allTeachers",
+          requestOptions
+        );
+        const json = await response.json();
+        setData(json);
+        console.log(json, "data++++++++++++++");
+        setRowCount(json.length);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+        return;
+      }
+      setIsError(false);
+      setIsLoading(false);
+      setIsRefetching(false);
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    columnFilters,
+    globalFilter,
+    pagination.pageIndex,
+    pagination.pageSize,
+    sorting,
+  ]);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "first_name",
+        header: "First Name",
       },
-      MuiOutlinedInput: {
-        root: {
-          background: "#F9FAFF 0% 0% no-repeat",
-          borderRadius: "6px",
-          height: "44px",
-        },
+      //column definitions...
+      {
+        accessorKey: "last_name",
+        header: "Last Name",
       },
+      // {
+      //   accessorKey: "address",
+      //   header: "Address",
+      // },
+      // {
+      //   accessorKey: "state",
+      //   header: "State",
+      // },
+      // {
+      //   accessorKey: "phoneNumber",
+      //   header: "Phone Number",
+      // },
+      //end
+    ],
+    []
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    // enableRowSelection: true,
+    enableColumnActions: false,
+    enableColumnFilters: false,
+    // enablePagination: false,
+    // enableSorting: false,
+    enableColumnResizing: true,
+    muiTableBodyProps: {
+      sx: {
+        //stripe the rows, make odd rows a darker color
+        // "& td:nth-of-type(odd)": {
+        //   backgroundColor: "#f5f5f5",
+        // },
+        backgroundColor: "#f5f5f5",
+      },
+    },
+    muiTableBodyCellProps: {
+      sx: {
+        borderRight: "2px solid #e0e0e0", //add a border between columns
+      },
+    },
+
+    getRowId: (row) => row.phoneNumber,
+    initialState: { showColumnFilters: true },
+    manualFiltering: true,
+    manualPagination: true,
+    manualSorting: true,
+    muiToolbarAlertBannerProps: isError
+      ? {
+          color: "error",
+          children: "Error loading data",
+        }
+      : undefined,
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+    rowCount,
+    state: {
+      columnFilters,
+      globalFilter,
+      isLoading,
+      pagination,
+      showAlertBanner: isError,
+      showProgressBars: isRefetching,
+      sorting,
     },
   });
 
-  const handleFiltersModal = () => {
-    setOpen(true);
-  };
-  const onClose = () => {
-    setOpen(false);
-  };
-
-  React.useEffect(() => {
-    if (payout.lastCalcDate) {
-      const content = get(payout.lastCalcDate, "data", []);
-      const { count } = payout.lastCalcDate;
-      setLastCalculationData(content);
-      setPageDetails({
-        ...pageDetails,
-        lastPage: Math.ceil(count / rowsPerPage),
-        from: page === 1 ? 1 : (page - 1) * rowsPerPage + 1,
-        to: page * rowsPerPage < count ? page * rowsPerPage : count,
-        total: count,
-      });
-      setRowsPerPage(count);
-    }
-  }, [payout.lastCalcDate]);
-
-  return (
-    <>
-      <div>
-        Last calculation date sada :{" "}
-        <span
-          onClick={handleFiltersModal}
-          style={{ cursor: "pointer", color: "#0093c9" }}
-        >
-          {`${lastCalcData[0]?.LastCalcMonthYear} (${lastCalcData[0]?.ProcessorName})`}
-        </span>
-      </div>
-      <Modal
-        open={open}
-        onClose={onClose}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        aria-labelledby="sasdasd"
-        aria-describedby="simpleasdasdasdan"
-      >
-        <Fade in={open}>
-          <ThemeProvider theme={theme}>
-            <Paper
-              style={{
-                width: "35%",
-                height: "60%",
-                padding: theme.spacing(2, 4, 3),
-              }}
-            >
-              <Grid
-                style={{
-                  justifyContent: "space-between",
-                }}
-                spacing={4}
-                container
-              >
-                <Grid item>Count : {lastCalcData.length}</Grid>
-                <Grid item>
-                  <Typography style={{ textDecorationLine: "underline" }}>
-                    Last Calculation By Processor
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <CloseIcon onClick={onClose} style={{ cursor: "pointer" }} />
-                </Grid>
-              </Grid>
-
-              <TableContainer
-                className={classes.TableContainer}
-                component={Paper}
-                style={{ margin: "2rem 0" }}
-              >
-                <CustomTable
-                  page={page} /// done using useSate
-                  order={order} /// done using useSate
-                  orderBy={orderBy} /// done using useSate
-                  setOrder={setOrder} /// done using useSate
-                  setOrderBy={setOrderBy} /// done using useSate
-                  data={lastCalcData}
-                  headCells={headCellsItems}
-                  dataParameter={false}
-                  processorParameter={false}
-                  isCallInitialization={initialCall}
-                  selected={false}
-                  setSelected={false}
-                  isSelection={false}
-                  rowsPerPage={rowsPerPage} /// done using useSate
-                  pageDetails={pageDetails} /// done using useSate
-                  handleChangePage={handleChangePage}
-                  handleChangeRowsPerPage={handleChangeRowsPerPage}
-                  totalCount={pageDetails.total} /// done using useSate
-                  paginationFlag={false}
-
-                  // const classes = useStyles();
-                  // const oldTheme = useTheme();
-                  // const { enqueueSnackbar } = useSnackbar();
-                  // const [open, setOpen] = React.useState(false);
-                  // const [lastCalcData, setLastCalculationData] = React.useState([]);
-                  // const [page, setPage] = React.useState(1);
-                  // const [initialCall, setInitialCall] = React.useState(true);
-                  // const [pageDetails, setPageDetails] = React.useState({ ...TablePageData });
-                  // const [order, setOrder] = React.useState("desc");
-                  // const [orderBy, setOrderBy] = React.useState("ProcessorName");
-                  // const [rowsPerPage, setRowsPerPage] = React.useState(5);
-                >
-                  {lastCalcData.map((row) => {
-                    return (
-                      <TableRow className={classes.cellHeadSign}>
-                        <TableCell className={classes.cellText} align="left">
-                          {row.ProcessorName}
-                        </TableCell>
-                        <TableCell align="center" className={classes.cellText}>
-                          {row.LastCalcMonthYear}
-                        </TableCell>
-                        <TableCell align="center" className={classes.cellText}>
-                          {row.Status}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </CustomTable>
-              </TableContainer>
-            </Paper>
-          </ThemeProvider>
-        </Fade>
-      </Modal>
-    </>
-  );
+  return <MaterialReactTable table={table} />;
 };
 
-SearchEntity.propTypes = {};
-
-SearchEntity.defaultProps = {};
-const mapStateToProps = ({ payout }) => ({
-  payout,
-});
-
-export default connect(mapStateToProps, {})(SearchEntity);
+export default SearchEntity;
