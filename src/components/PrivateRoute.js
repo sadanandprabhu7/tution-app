0027caf87b9a5a967a31dcae591f0b81db807a07
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2024-03-01 16:35:57 +0530

    api backend seperated from fe

--- src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
-export default function PrivateRoute() {
-  // const auth = useSelector((state) => state.auth);
-  const auth = localStorage.getItem("token");
-  return auth ? <Outlet /> : <Navigate to="/LogIn" />;
-}
